aefbf00dd563d4f04601f66e639d86403e1a01f0
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2021-11-15 23:24:38 +0100

    this should work

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,19 +1,29 @@
 const form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
-	searchCategory(e);
+	e.preventDefault();
+	const input = form.search.value.trim();
+	searchCategory(input);
+	form.reset();
 });
 
-function searchCategory(e) {
-	e.preventDefault();
-
-	let input = form.search.value.trim();
-
-	// data.filter(collection => {
-	// 	const filteredData = collection.category === "Mountain";
-	// 	form.reset(); 
-	// 	form.innerHTML += data; 
-	// 	data = filteredData;
-	// });
-	// reset form 
+function searchCategory(input) {
+	const filtered = data.filter(collection => collection.category === input.toLowerCase());
+	
+	filtered.forEach(filter => {
+		let html =``;
+        html = `
+            <div class="image-gallery">
+                <div class="option">
+                    <h3>${filter.name}</h3>
+                    <a href="${filter.url}" download="pexel-pixar-${filter.id * 567}">Download</a>
+                </div>
+                <div class="gallery-overflow">
+                    <img src="${filter.url}" alt="Mountain">
+                </div> 
+                <button style="background:${filter.color}" class="color">${filter.color}</button>
+            </div>
+        `;
+        gallery.innerHTML += html;
+	})
 }
